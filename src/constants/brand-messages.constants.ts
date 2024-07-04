fcc73b4015a121ac042d56d6714a8a945b8e74ba
evdmatvey <evd.matvey@mail.ru>
2024-07-04 16:07:24 +0700

    Add brand constants

---
+++ src/constants/brand-messages.constants.ts
@@ -0,0 +1,5 @@
+export const BrandMessages = {
+  BRAND_DELETE_SUCCESS: 'Бренд успешно удалён!',
+  BRAND_UPDATE_SUCCESS: 'Бренд успешно обновлён!',
+  BRAND_CREATE_SUCCESS: 'Бренд успешно создан!',
+};
