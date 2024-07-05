046a372fa42b034cffde37db4abe58e40399d9b2
evdmatvey <evd.matvey@mail.ru>
2024-07-05 18:56:44 +0700

    Add super category constants

---
+++ src/constants/super-category-errors.constants.ts
@@ -0,0 +1,6 @@
+export const SuperCategoryErrors = {
+  SUPER_CATEGORY_ALREADY_EXIST: 'Такая супер категория уже есть в системе!',
+  SUPER_CATEGORY_NOT_FOUND: 'Супер категория не найдена!',
+  SUPER_CATEGORY_SUB_CATEGORY_NOT_FOUND:
+    'Супер подкатегория не найдена в супер категории!',
+};
