e6a3357f200f130ef5b34d9e9ab3b590136eec42
evdmatvey <evd.matvey@mail.ru>
2024-07-11 21:24:26 +0700

    Set up internationalization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import * as path from 'path';
+
 import {
   AuthModule,
   BrandModule,
@@ -13,6 +16,15 @@ import {
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'ru',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [new HeaderResolver(['x-custom-lang'])],
+      typesOutputPath: path.join(__dirname, '/generated/i18n.generated.ts'),
+    }),
     UserModule,
     AuthModule,
     CategoryModule,
